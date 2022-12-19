5f3d122edfd77cb3c917575e4eb50280fed6a735
KsenoMorFys <dannut055@yandex.ru>
2022-12-19 14:03:22 +0300

    abs_fabs_ceil/add_all_func

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,1 +1,29 @@
-//sdadssfdsfdfdfd
+#include "s21_math.h"
+
+// Abs, fabs, ceil, floor, fmod functions
+int s21_abs(int x){
+    return (x > 0) ? x : -x;
+}
+
+long double s21_fabs(double x){
+    return (x > 0) ? x : -x;
+}
+
+long double s21_ceil(double x){
+    return ((x - (int)x) && x > 0.) ? (long double)((int)x + 1) : (long double)(int)x;
+}
+
+long double s21_floor(double x){
+    return ((x - (int)x) && x < 0.) ? (long double)((int)x - 1) : (long double)(int)x;
+}
+
+long double s21_fmod(double x, double y){
+    long double res = s21_fabs(x);
+    y = s21_fabs(y);
+    int i = 1;
+    while (res >= y){
+        res = s21_fabs(x) - y * i;
+        i++;
+    }
+    return x < 0 ? - res : res;
+}
