34876b01d4799cf8313dbcc4d4de0861db95eee7
KsenoMorFys <dannut055@yandex.ru>
2022-12-19 14:17:26 +0300

    pow_sqrt/добавил pow;sqrt

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,1 +1,25 @@
-//sdadssfdsfd
+#include "s21_math.h"
+
+
+// Pow and sqrt functions
+
+long double s21_pow(double base, double ex) {
+    long double ans = 0.0;
+    if (ex == (int)ex) {
+        if (ex < 0) {
+            base = 1 / base;
+            ex = -ex;
+        }
+        ans = 1.;
+        for (int i = 0; i < ex; i++) {
+            ans *= base;
+        }
+    } else {
+        ans = exp(ex * log(base));
+    }
+    return ans;
+}
+
+long double s21_sqrt(double x) {
+    return s21_pow(x, 0.5);
+}
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,1 +1,11 @@
-//sdadsadsfsdfd
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <check.h>
+#include <ctype.h>
+#include <stdint.h>
+
+// Pow and sqrt functions
+
+long double s21_sqrt(double sqrt);
+long double s21_pow(double base, double exp);
