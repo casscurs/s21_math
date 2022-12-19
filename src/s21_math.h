1200ccb1899a4401570fd3ca5666ad8c648b8c6c
KsenoMorFys <dannut055@yandex.ru>
2022-12-19 14:01:57 +0300

    abs_fabs_ceil/add_all_func

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,1 +1,14 @@
-//sdadsadsfsdsdsdadfd
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <check.h>
+#include <ctype.h>
+#include <stdint.h>
+
+// Abs, fabs, ceil, floor, fmod functions
+
+int s21_abs(int x);
+long double s21_fabs(double x);
+long double s21_ceil(double x);
+long double s21_floor(double x);
+long double s21_fmod(double x, double y);
